12201ba666a2ddbe43274fd522c66b81784a5e4a
fpopescu <popescu01@gmail.com>
2023-12-06 05:57:28 -0600

    Lesson_08

--- fetch.js
+++ fetch.js
@@ -1,32 +1,47 @@
 // ./fetch.js
 
-// !!! UNDER WORK !!!
-
 /* --------------------------------------------- */
 // create api-key.js file with const API_KEY="your_api_key" in this same directory to use
 const API_KEY = "eh7c80hvSfet5CLcSYeZRrSGLzq4AWYX";
 
-const BASE_URL = 'https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json';
-//const BOOKS_URL = "https://api.nytimes.com/svc/books/v3/lists/{2022-03-12}/{hardcover-fiction}.json";
+//const BASE_URL = 'https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json';
+const BOOKS_URL = "https://api.nytimes.com/svc/books/v3/lists/best-sellers/history.json";
 
 /* --------------------------------------------- */
 const url = `${BOOKS_URL}?api-key=${API_KEY}`;
 
 fetch(url)
-  .then(function(data) {
-    return data.json();
-  })
-  .then(function(responseJson) {
-    console.log(responseJson);
+    .then(function (data) {
+        return data.json();
+    })
+    .then(function (responseJson) {
+        let articles = responseJson.results;
 
-    let article = responseJson.response.docs[0];
-    console.log(article);
+        let first5 = articles.slice(0, 5);
+        console.log(first5);
 
-    const mainHeadline = article.headline.main;
-    document.getElementById('article-title').innerText = mainHeadline;
+        first5.forEach(article => {
+            let content = document.getElementById("container");
 
-    if (article.multimedia.length > 0) {
-      const imgUrl = `https://www.nytimes.com/${article.multimedia[0].url}`;
-      document.getElementById('article-img').src = imgUrl;
-    }
-  });
+            let articleTitle = article.title;
+            let titleParagraph = document.createElement('h3');
+            titleParagraph.style.backgroundColor = 'yellow';
+            titleParagraph.style.color = 'red';
+            let titleNode = document.createTextNode(`Title: ${articleTitle}`);
+            titleParagraph.appendChild(titleNode);
+            content.appendChild(titleParagraph);
+
+            let articleAuthor = article.author;
+            let authorParagraph = document.createElement('h3');
+            let authorNode = document.createTextNode(`Author: ${articleAuthor}`);
+            authorParagraph.appendChild(authorNode);
+            content.appendChild(authorParagraph);
+
+            let articleDescription = article.description;
+            let descriptionParagraph = document.createElement('h4');
+            descriptionParagraph.style.fontStyle = "italic";
+            let descriptionNode = document.createTextNode(`Description: ${articleDescription}`);
+            descriptionParagraph.appendChild(descriptionNode);
+            content.appendChild(descriptionParagraph);
+        })
+    });
